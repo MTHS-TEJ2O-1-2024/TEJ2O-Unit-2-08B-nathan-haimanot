9892d6f13a6cee755f6ccd48cdadc4ec6d12180f
nathan-haimanot <nathan.haimanot@mths.ca>
2024-10-03 16:57:58 -0400

    Made a RGB light

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nathan 
+ * Created on: Oct 2024
+ * This program switches the RGB colour.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//turns on leds
+input.onButtonPressed(Button.A, function(){
+    //red
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    basic.pause(1000)
+    //blue
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin. P15, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.pause(1000)
+    //green
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1000)
+    //all
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+})
+
+    //turn off leds
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0) 
+})
